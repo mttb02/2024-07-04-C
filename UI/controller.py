71bed713064b7b6a9939556d833ccb320c7edfc9
Giuseppe Averta <g.averta3@gmail.com>
2024-07-03 15:35:08 +0200

    first commit

---
+++ UI/controller.py
@@ -0,0 +1,23 @@
+import flet as ft
+from UI.view import View
+from model.modello import Model
+
+
+class Controller:
+    def __init__(self, view: View, model: Model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+
+    def handle_graph(self, e):
+        pass
+
+    def handle_path(self, e):
+        pass
+
+    def fill_ddyear(self):
+        pass
+
+    def fill_ddshape(self, e):
+        pass
